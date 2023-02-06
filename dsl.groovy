0d0b69e07f37734baf094e413583d26b0775760b
dani_github <danifpgm@gmail.com>
2023-02-06 12:57:19 +0100

    first commit

---
+++ dsl.groovy
@@ -0,0 +1,22 @@
+// import jenkins-auto-approve-script.groovy
+
+job('pruebaDSL') {
+    description('Descripcion')
+
+
+
+    scm {
+        git('https://github.com/danifpgm/jenkins-prueba.git', 'master'){ 
+            node -> 
+                node / gitConfigName('danifpgm')
+                node / gitConfigEmail('danifpgm@gmail.com')
+        }
+    }
+    triggers {
+        githubPush()
+    }
+    
+    steps {
+        shell("bash install.sh")
+    }
+}
